7cbf9396fb14385de14d765fd9592d12e4c04ae1
hemantch01 <hemantchaudhary905@gmail.com>
2025-09-27 07:48:07 +0000

    added some part to readme

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -2,8 +2,19 @@ export const FooterSection = () => {
     return <div className="layout_2">
         {/*main card*/}
         <div className="footer_main_card">
-         heello boa
+         {/*inside content main*/}
+         <div className="below_section_2_renew">
+            Set up in 10 mins.
+         </div>
+         <div className="below_section_2_renew_moreGrey">
+            Back to building by 12:00AM.
+         </div>
+         {/*inside content main*/}
+         <div className="footer_main_smalltext">
+            Built for founders who want to focus on their business, not their accounting.
+         </div>
+         {/*main button */}
+         <button className="nav_button">Get started</button>
         </div>
-
     </div>
 }
